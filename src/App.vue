0e51cfea8bde1e1dc7aa7c56a0f1a16ab763419b
GOLF <golf.prptmk@gmail.com>
2019-01-29 23:16:16 +0700

    Maps section edited

--- src/App.vue
+++ src/App.vue
@@ -58,6 +58,7 @@
         >
         </textdiv>
         <gmaps/>
+        
       </div>
 
     </div>
--- src/components/gmaps.vue
+++ src/components/gmaps.vue
@@ -1,30 +1,21 @@
 <template>
   <div>
-    <div>
-      <h2>Search and add a pin</h2>
-      <label>
-        <gmap-autocomplete
-          @place_changed="setPlace">
-        </gmap-autocomplete>
-        <button @click="addMarker">Add</button>
-      </label>
-      <br/>
-
+  <GmapMap
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 300px"
+  >
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    />
+  </GmapMap>
     </div>
-    <br>
-    <gmap-map
-      :center="center"
-      :zoom="12"
-      style="width:100%;  height: 400px;"
-    >
-      <gmap-marker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        @click="center=m.position"
-      ></gmap-marker>
-    </gmap-map>
-  </div>
 </template>
 
 <script>
