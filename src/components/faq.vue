f7119c5ec22af68e01b80028415447e2be5d0455
Peerapong Thammakaew (Golf) <golf.prptmk@gmail.com>
2019-01-31 02:15:43 +0700

    logos

---
+++ src/components/faq.vue
@@ -0,0 +1,62 @@
+<template>
+<div style="overflow: hidden; border-radius: 20px;">
+<el-collapse accordion>
+
+  <el-collapse-item name="1">
+    <template slot="title">
+      <p class="txt-question"> {{ $t("message.faq.question1.q") }}</p>
+    </template>
+    <p class="txt-answer">{{ $t("message.faq.question1.a") }}</p>
+  </el-collapse-item>
+
+  <el-collapse-item name="2">
+    <template slot="title">
+      <p class="txt-question"> {{ $t("message.faq.question2.q") }} </p>
+    </template>
+    <p class="txt-answer">{{ $t("message.faq.question2.a") }}<a href=“”https://facebook.com/unzipEvent> Facebook </a></p>
+  </el-collapse-item>
+
+  <el-collapse-item name="3">
+    <template slot="title">
+      <p class="txt-question"> {{ $t("message.faq.question3.q") }} </p>
+    </template>
+    <p class="txt-answer">{{ $t("message.faq.question3.a") }}</p>
+  </el-collapse-item>
+
+  <el-collapse-item name="4">
+    <template slot="title">
+      <p class="txt-question"> {{ $t("message.faq.question4.q") }} </p>
+    </template>
+    <p class="txt-answer">{{ $t("message.faq.question4.a") }}</p>
+  </el-collapse-item>
+
+
+</el-collapse>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'faq'
+}
+</script>
+
+<style scoped>
+.txt-question{
+  margin: 10px;
+  font-family: 'Kanit', sans-serif;
+  font-size: 19px;
+  color: #434547;
+}
+.txt-answer{
+  margin: 10px;
+  font-family: 'Kanit', sans-serif;
+  font-size: 17px;
+  color: #606266;
+}
+.el-collapse-item__header {
+
+  height: 60px !important;
+
+}
+</style>
---
+++ src/components/sponsor.vue
@@ -0,0 +1,105 @@
+<template>
+<div>
+  <el-carousel :interval="4000" indicator-position="none" type="card" height="300px">
+    <el-carousel-item v-for="item in 1" :key="item">
+      <div class="sponsor d-flex align-items-center justify-content-center flex-column">
+        <img src="/static/sponsor/angular.png" style="width: 35%;" />
+        <h5 style="margin: 20px; text-align: center;">{{ $t("message.sponsor.spon1") }}</h5>
+      </div>
+    </el-carousel-item>
+    <el-carousel-item v-for="item in 1" :key="item">
+      <div class="sponsor d-flex align-items-center justify-content-center flex-column">
+        <img src="/static/sponsor/kmutt.png" style="width: 35%;" />
+        <h5 style="margin: 20px; text-align: center;">{{ $t("message.sponsor.spon2") }}</h5>
+      </div>
+    </el-carousel-item>
+    <el-carousel-item v-for="item in 1" :key="item">
+      <div class="sponsor d-flex align-items-center justify-content-center flex-column">
+        <img src="/static/sponsor/cpe.png" style="width: 35%;" />
+        <h5 style="margin: 20px; text-align: center;">{{ $t("message.sponsor.spon3") }}</h5>
+      </div>
+    </el-carousel-item>
+  </el-carousel>
+  <div class="row" style="margin-top: 20px;">
+    <div class="col-md-2 d-flex align-items-center justify-content-center">
+      <img src="/static/sponsor/cpe.png" style="height: 80px;" />
+    </div>
+    <div class="col-md-2 d-flex align-items-center justify-content-center">
+      <img src="/static/sponsor/kmutt.png" style="height: 80px;" />
+    </div>
+    <div class="col-md-2 d-flex align-items-center justify-content-center">
+      <img src="/static/sponsor/angular.png" style="height: 80px;" />
+    </div>
+    <div class="col-md-2 d-flex align-items-center justify-content-center">
+      <img src="/static/sponsor/cpe.png" style="height: 80px;" />
+    </div>
+    <div class="col-md-2 d-flex align-items-center justify-content-center">
+      <img src="/static/sponsor/kmutt.png" style="height: 80px;" />
+    </div>
+    <div class="col-md-2 d-flex align-items-center justify-content-center">
+      <img src="/static/sponsor/angular.png" style="height: 80px;" />
+    </div>
+  </div>
+  
+</div>
+</template>
+<script>
+export default {
+  name: 'sponsor'
+}
+</script>
+
+<style scoped>
+.el-carousel__item {
+  border-radius: 20px;
+  overflow: hidden;
+  width: 100%;
+  display: inline-block;
+  overflow: hidden;
+  z-index: 0;
+}
+
+.el-carousel__item.is-active {
+  z-index: 2;
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+.el-carousel__item.is-animating {
+  -webkit-transition: -webkit-transform .4s ease-in-out;
+  transition: -webkit-transform .4s ease-in-out;
+  transition: transform .4s ease-in-out;
+  transition: transform .4s ease-in-out, -webkit-transform .4s ease-in-out;
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+.el-carousel__item--card {
+  width: 50%;
+  -webkit-transition: -webkit-transform .4s ease-in-out;
+  transition: -webkit-transform .4s ease-in-out;
+  transition: transform .4s ease-in-out;
+  transition: transform .4s ease-in-out, -webkit-transform .4s ease-in-out;
+  border-radius: 20px;
+  overflow: hidden;
+}
+  .sponsor{
+    width: 100%;
+    height: 100%;
+  }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
